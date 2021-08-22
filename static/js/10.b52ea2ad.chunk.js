852d755558a8de878099c2ab825db57ce4582b9d
nkpn <illinmikita@gmail.com>
2021-08-22 12:48:17 +0300

    Updates

---
+++ static/js/10.b52ea2ad.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-05-movies"]=this["webpackJsonpgoit-react-hw-05-movies"]||[]).push([[10],{196:function(t,e,i){"use strict";i.r(e),i.d(e,"default",(function(){return c}));var n=i(1);function c(){return Object(n.jsx)("h1",{children:"404 \u0421\u0442\u0440\u0430\u043d\u0438\u0446\u0430 \u043d\u0435 \u043d\u0430\u0439\u0434\u0435\u043d\u0430 :("})}}}]);
+//# sourceMappingURL=10.b52ea2ad.chunk.js.map
